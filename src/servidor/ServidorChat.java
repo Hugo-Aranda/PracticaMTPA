00c3bae17f6eb68af3a9c99df0f91d8ec1496947
Hugo Aranda Alija <haranda33221@alumnos.uemc.es>
2026-05-12 09:26:00 +0200

    Implementado servidor base

---
+++ src/servidor/ServidorChat.java
@@ -0,0 +1,47 @@
+package servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorChat {
+
+	private static final int PUERTO = 1234;
+
+	public static void main(String[] args){
+
+		GestroClientes gestorClientes = new GestorClientes();
+		GestorSalones gestorSalones = new GestorSalones();
+
+		try {
+			ServerSocket servidor = new ServerSocket(PUERTO);
+
+
+			System.out.println("Servidor iniciado en el puerto "+ PUERTO);
+
+			while(true){
+				Socket socketCliente = servidor.accept();
+
+				System.out.println("Nuevo cliente conectado");
+
+			}
+
+
+		}catch(IOException e){
+
+			System.out.println("Error en el servidor");
+
+		}
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
